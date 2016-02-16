1298286909206a1a65cf0297b468e2f1feb21c1d
Milan Falešník <mfalesni@redhat.com>
2016-02-16 16:32:37 +0100

    Relicense and update setup.py

--- setup.py
+++ setup.py
@@ -9,17 +9,24 @@ from setuptools import setup
 
 setup(
     name="deprecate",
-    version="1.0.1",
+    version="1.0.2",
     author="Milan Falešník",
     author_email="milan@falesnik.net",
-    description="Python function kwargifier",
-    license="GPLv3",
-    keywords="kwargs",
+    description="Python deprecating solution.",
+    license="GPLv3,MPLv2",
+    keywords="deprecate",
     url="https://github.com/mfalesni/python-deprecate",
     py_modules=["deprecate"],
     install_requires=[],
     classifiers=[
         "Topic :: Utilities",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Intended Audience :: Developers",
+        "Development Status :: 4 - Beta",
     ]
 )

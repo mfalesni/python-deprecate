737294c04a1f9451c8bddc2a6acc9a480d53f347
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2016-02-17 12:17:46 +0100

    add a test for computing deprecation messages

--- testing/test_deprecate.py
+++ testing/test_deprecate.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import warnings
 
-from deprecate import deprecated
+from deprecate import deprecated, DeprecationDecorator
+
+import pytest
 
 
 @deprecated
@@ -17,3 +19,14 @@ def test_warns(recwarn):
     assert issubclass(w.category, DeprecationWarning)
     assert str(w.message) == "Call to deprecated function {}".format(
         deprecated_function.__name__)
+
+
+class TestDeprecationDecorator:
+    @pytest.mark.parametrize('message, expected', [
+        (None, 'Call to deprecated function deprecated_function'),
+        ("Test", "Test"),
+    ])
+    def test_make_message(self, message, expected):
+        decorator = DeprecationDecorator(message=message)
+        computed_message = decorator._message_for_func(deprecated_function)
+        assert computed_message == expected
